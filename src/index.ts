19edafc61f56e271393aa53eaecb21bc020f53e4
Wittenbols <UMWOJ9@AMER.Corp.Local>
2025-02-12 10:28:20 -0800

    Include build

--- src/index.ts
+++ src/index.ts
@@ -2475,7 +2475,7 @@ function stripParams(url: URL) {
   url = new URL(url)
   //2025/2/12: Querystring are supported in some IAM platforms, so we shouldn't strip Redurect URIs of them
   // url.search = ''
-  url.hash = ''
+  // url.hash = ''
   return url.href
 }
 
